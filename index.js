a765b1252d4002e2f1a48005a3ccbbc9cac28a00
Brandon Ryan <bjryan19@protonmail.com>
2022-01-24 04:23:55 -0600

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+export function overrideWebpackConfig({webpackConfig, cracoConfig, pluginOptions}) {
+    if (webpackConfig.plugins === undefined) {
+        webpackConfig.plugins = []
+    }
+
+    webpackConfig.plugins.push(pluginOptions.plugins)
+}
